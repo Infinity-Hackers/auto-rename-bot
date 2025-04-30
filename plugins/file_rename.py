d5152994796a52579b42962ada2e79c8e18ac65a
Google <58548492+Infinity-Hackers@users.noreply.github.com>
2025-04-29 22:43:06 -0700

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -285,11 +285,3 @@ async def auto_rename_files(client, message):
 
 # Remove the entry from renaming_operations after successful renaming
         del renaming_operations[file_id]
-
-
-
-
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Developer @JishuDeveloper
